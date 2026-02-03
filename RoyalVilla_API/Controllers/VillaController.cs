d1157b3b7b6e24cea24440734cd2b64110e3840a
karim <karim@KARIM>
2026-02-03 22:59:47 +0200

    Get Villas and Get Villa By ID endpoints

--- RoyalVilla_API/Controllers/VillaController.cs
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RoyalVilla_API.Data;
 
 namespace RoyalVilla_API.Controllers
 {
@@ -7,6 +9,37 @@ namespace RoyalVilla_API.Controllers
     [ApiController]
     public class VillaController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetVillas()
+        {
+            return Ok(await _context.Villas.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVillaById(int id)
+        {
+            try
+            {
+                if(id == 0)
+                    return BadRequest($"Invalid villa ID {id}");
+
+                var villa = await _context.Villas.FirstOrDefaultAsync(v => v.Id == id);
+                if(villa == null)
+                    return NotFound($"Villa with ID {id} not found");
+                return Ok(villa);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"An error occured while retrieving villa with ID {id} : {ex.Message}");
+            }
+        }
     }
 }
